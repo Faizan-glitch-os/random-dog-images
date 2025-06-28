16d51b6df46b3c4cc967c52213b22166c1000fd2
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-06-28 17:35:28 +0500

    styling done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import { motion } from "motion/react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className=" h-[100vh] w-[100vw] flex flex-col items-center justify-center">
+      <form className="mt-8 flex flex-col md:flex-row space-y-4 md:space-x-4">
+        <input
+          type="text"
+          name="dog"
+          id="dog"
+          placeholder="Enter a dog breed"
+          className="mb-0 focus:outline-none border border-cyan-600 rounded placeholder:text-cyan-200/50 p-2"
+        />
+        <motion.button
+          whileHover={{ scale: 1.1, y: -8 }}
+          whileTap={{ scale: 0.9, y: 2 }}
+          className="bg-cyan-600 hover:bg-cyan-700 rounded text-xl text-white hover:text-amber-50 px-4 py-2 w-full"
+        >
+          Search
+        </motion.button>
+      </form>
+      <div className="mt-8 grid grid-rows-4 gap-2 md:grid-cols-2 md:grid-rows-2 md:gap-2">
+        <div className="bg-amber-600 rounded w-48 h-48">hello</div>
+        <div className="bg-amber-600 rounded w-48 h-48">hello</div>
+        <div className="bg-amber-600 rounded w-48 h-48">hello</div>
+        <div className="bg-amber-600 rounded w-48 h-48">hello</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
